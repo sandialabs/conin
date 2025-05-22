32168557fccb6e1ac085d2ef2b3b5c2b49632e35
clmatte <clmatte@sandia.gov>
2025-05-22 08:53:15 -0700

    Recursive A*

--- conin/hmm/inference/recursive_a_star.py
+++ conin/hmm/inference/recursive_a_star.py
@@ -1,4 +1,7 @@
 import heapq
+import time
+import numpy as np
+import munch
 
 
 class Recursive_Heap_Item:
@@ -92,7 +95,7 @@ class Recursive_Heap_Item:
         return (self.last_element, self.length, self.constraint_data)
 
 
-class Unique_Heap:
+class Unique_Heapq:
     """
     This is used in our tailored A* algorithm
     We consider to elements "equal" if their sequences end with the same
@@ -142,3 +145,178 @@ class Unique_Heap:
         Length of the heapq
         """
         return len(self.__heap)
+
+
+def recursive_a_star(
+    *,
+    hmm_app,
+    observed,
+    num_solutions=1,
+    debug=False,
+    max_iterations=None,
+    max_time=None,
+):
+    """
+    This is a tailored version of the A* algorithm -- copied and modified
+    from conin.hmm.inference.viterbi. 
+    
+    Paramters:
+        hmm_app: HMM_Application
+        observed: List of observed states
+        num_solutions: Number of solutions we wish to find
+        debug: Whether to have additional printing
+        max_iterations: Maximum number of iterations
+        max_time: Maximum time of algorithm
+    """
+    start_time = time.time()
+    knowledge_state_times_output = []
+    obj_vals = []
+
+    # Initalize variables
+    hmm = hmm_app.get_hmm()
+    time_steps = len(observed)
+
+    start_probs = hmm.get_start_probs()
+    emission_probs = hmm.get_emission_probs()
+    transition_probs = hmm.get_transition_probs()
+    hidden_states = hmm.get_hidden_states()
+
+    # Precompute log probabilities for emission and transmission matrices
+    log_emission_probs = {
+        (h, o): np.log(emission_probs[(h, o)])
+        for (h, o) in emission_probs.keys()
+        if emission_probs[(h, o)] > 0
+    }
+    log_transition_probs = {
+        (h1, h2): np.log(transition_probs[(h1, h2)])
+        for (h1, h2) in transition_probs.keys()
+        if transition_probs[(h1, h2)] > 0
+    }
+
+    # Precompute V[t][h] - The log-probability of the shortest path starting at time
+    #       t in hidden state h
+    V = {(t, h): 0 for t in range(time_steps) for h in hidden_states}
+
+    for t in range(time_steps - 2, -1, -1):
+        o = observed[t + 1]
+        for h1 in hidden_states:
+            temp = np.inf
+            for h2 in hidden_states:
+                if (transition_probs[(h1, h2)] != 0) and (emission_probs[(h2, o)] != 0):
+                    temp = min(
+                        temp,
+                        V[(t + 1, h2)]
+                        - log_transition_probs[(h1, h2)]
+                        - log_emission_probs[(h2, o)],
+                    )
+            V[(t, h1)] = temp
+
+    get_gScore = dict() # Maps recursive heap item to negative log-probabilities
+    get_seq = dict() # Maps recursive heap item ids to sequences
+
+    openSet = Unique_Heapq()
+
+    # Initialize the heap with the starting states
+    for h in hidden_states:
+        if (
+            (h in start_probs.keys())
+            and (start_probs[h] > 0)
+            and ((h, observed[0]) in emission_probs.keys())
+            and (emission_probs[(h, observed[0])] > 0)
+        ):
+            gScore = -np.log(start_probs[h]) - log_emission_probs[(h, observed[0])]
+            constraint_data = hmm_app.initialize_constraint_data(h)
+
+            if hmm_app.constraint_data_feasible_partial(
+                constraint_data = constraint_data, t = 1
+            ):
+                item = Recursive_Heap_Item(priority=gScore + V[(0,h)], last_element=h, length=1, constraint_data=constraint_data)
+                
+                get_gScore[item] = gScore
+                openSet.add(item)
+                get_seq[item] = (h,)
+
+    iteration = 0
+    n_infeasible = 0
+    termination_condition = "unknown"
+    output = []
+        
+    while True:
+        item = openSet.pop()
+        val = item.priority
+        constraint_data = item.constraint_data
+        t = item.length
+        h1 = item.last_element
+        
+        gScore = get_gScore.pop(item)
+        seq = get_seq.pop(item)
+        
+        if t == time_steps:
+            if hmm_app.constraint_data_feasible(constraint_data):
+                output.append(munch.Munch(hidden=list(seq), log_likelihood=-val))
+                obj_vals.append(-val)
+                if len(output) == num_solutions:
+                    termination_condition = "ok"
+                    break
+                else:
+                    n_infeasible += 1
+
+        else:
+            obs = observed[t]
+            for h2 in hidden_states:
+                if (
+                    emission_probs.get((h2, obs), 0.0) == 0.0
+                    or transition_probs.get((h1, h2), 0.0) == 0.0
+                ):
+                    continue
+                new_constraint_data = hmm_app.update_constraint_data(hidden_state = h2, constraint_data = constraint_data)
+                if hmm_app.constraint_data_feasible_partial(constraint_data = constraint_data, t = t):
+                    new_gScore = (
+                        gScore
+                        - log_transition_probs[(h1, h2)]
+                        - log_emission_probs[(h2, obs)]
+                    )
+                    
+                    new_item = Recursive_Heap_Item(priority=new_gScore + V[(t, h2)], last_element=h2, length=t+1, constraint_data=new_constraint_data)
+                    
+                    get_gScore[new_item] = new_gScore
+                    openSet.add(new_item)
+                    get_seq[new_item] = seq+(h2,)
+
+        iteration += 1
+        if (max_iterations is not None) and (iteration >= max_iterations):
+            termination_condition = f"max_iterations: {iteration}"
+            break
+
+        curr_time = time.time()
+        if (max_time is not None) and ((curr_time - start_time) > max_time):
+            termination_condition = f"max_time: {curr_time-start_time}"
+            break
+
+        if openSet == []:
+            break
+
+        if debug:
+            if iteration % 100 == 0:
+                print(f"  Iteration: {iteration}")
+                print(f"  # Heap:    {len(openSet)}")
+                print(f"  t:         {t}")
+                print(f"  val:       {val}")
+                print(f"  ninfeas:   {n_infeasible}")
+                print(f"  time:      {curr_time-start_time}")
+
+    if len(output) < num_solutions:
+        if num_solutions == 1:
+            termination_condition = "error: no feasible solutions"
+        else:
+            termination_condition = "ok"
+
+    ans = munch.Munch(
+        observations=observed,
+        solutions=output,
+        termination_condition=termination_condition,
+        knowledge_state_times=knowledge_state_times_output,
+        objective_values=obj_vals,
+    )
+
+    return ans
--- conin/hmm/tests/test_recursive_a_star.py
+++ conin/hmm/tests/test_recursive_a_star.py
@@ -1,32 +1,114 @@
 import pytest
+import random
 
 from conin.hmm.inference.recursive_a_star import *
+from conin.hmm import *
+from conin.hmm.inference import a_star
+from conin import *
 
+
+class Num_Zeros(HMMApplication):
+
+    def __init__(self):
+        self.num_zeros = None
+        super().__init__(self.__class__.__name__)
+
+    def initialize(
+        self,
+        *,
+        hmm=None,
+        prob_stay_in_same_state=None,
+        prob_error=None,
+        zero_start_prob=0.5,
+        num_zeros,
+        time
+    ):
+        if hmm is None:
+            start_probs = {0: zero_start_prob, 1: 1 - zero_start_prob}
+            transition_probs = {
+                (0, 0): prob_stay_in_same_state,
+                (0, 1): 1 - prob_stay_in_same_state,
+                (1, 0): 1 - prob_stay_in_same_state,
+                (1, 1): prob_stay_in_same_state,
+            }
+            emission_probs = {
+                (0, 0): 1 - prob_error,
+                (0, 1): prob_error,
+                (1, 0): prob_error,
+                (1, 1): 1 - prob_error,
+            }
+            hmm = HMM()
+            hmm.load_model(
+                start_probs=start_probs,
+                transition_probs=transition_probs,
+                emission_probs=emission_probs,
+            )
+            self.hmm = hmm
+        else:
+            self.hmm = hmm
+        self.num_zeros = num_zeros
+        self.generate_oracle_constraints()
+        self._hidden_states = {0, 1}
+        self._observable_states = {0, 1}
+        self.time = time
+
+    def run_simulations(
+        self, *, num=1, debug=False, seed=None, with_observations=False
+    ):
+        if seed is not None:
+            random.seed(seed)
+        output = []
+        for n in range(num):
+            res = munch.Munch()
+            hidden = self.oracle.generate_hidden(self.time)
+            if with_observations:
+                observed = self.oracle.generate_observed_from_hidden(hidden)
+            res = munch.Munch(hidden=hidden, index=n)
+            if with_observations:
+                res.observed = observed
+            output.append(res)
+        return output
+    
+    def generate_oracle_constraints(self):
+        constraint = has_exact_number_of_occurences_constraint(
+            val=0, count=self.num_zeros
+        )
+        self.oracle.set_constraints([constraint])
+    
+    def initialize_constraint_data(self, hidden_state):
+        if hidden_state == 0:
+            return 1
+        else:
+            return 0
+        
+    def constraint_data_feasible_partial(self, *, constraint_data, t):
+        return (constraint_data+(self.time-t) >= self.num_zeros) and constraint_data <= self.num_zeros
+    
+    def constraint_data_feasible(self, constraint_data):
+        return constraint_data == self.num_zeros
+    
+    def update_constraint_data(self, *, hidden_state, constraint_data):
+        if hidden_state == 0:
+            return constraint_data + 1
+        else:
+            return constraint_data
 
 @pytest.fixture
-def hmm():
-    start_probs = {"h0": 0.4, "h1": 0.6}
-    transition_probs = {
-        ("h0", "h0"): 0.9,
-        ("h0", "h1"): 0.1,
-        ("h1", "h0"): 0.2,
-        ("h1", "h1"): 0.8,
-    }
-    emission_probs = {
-        ("h0", "o0"): 0.7,
-        ("h0", "o1"): 0.3,
-        ("h1", "o0"): 0.4,
-        ("h1", "o1"): 0.6,
-    }
-    hmm = HMM()
-    hmm.load_model(
-        start_probs=start_probs,
-        transition_probs=transition_probs,
-        emission_probs=emission_probs,
+def app():
+    prob_stay_in_same_state = 0.6  # 1/(1-prob_stay_in_same_state) = expected number of iterations of the same state
+    prob_error = (
+        0.3  # Proability that hidden state h has an observation which does not match it
     )
-    hmm.set_seed(0)
-    return hmm
-
+    num_zeros = 10  # Number of zeros
+    time = 20
+    app = Num_Zeros()
+    app.initialize(
+        prob_stay_in_same_state=prob_stay_in_same_state,
+        prob_error=prob_error,
+        num_zeros=num_zeros,
+        time=time,
+    )
+    return app
 
 @pytest.fixture
 def heap_item():
@@ -122,7 +204,7 @@ class Test_Heap_Item:
 
 class Test_Unique_Heap:
     def test_init(self):
-        A = Unique_Heap()
+        A = Unique_Heapq()
 
     def test_add(self, heap_item):
         x2 = Recursive_Heap_Item(
@@ -135,7 +217,7 @@ class Test_Unique_Heap:
             priority=2, last_element="h1", length=10, constraint_data=(1, 2)
         )
 
-        A = Unique_Heap()
+        A = Unique_Heapq()
         A.add(heap_item)
         assert len(A) == 1
         A.add(x2)
@@ -152,7 +234,7 @@ class Test_Unique_Heap:
         assert len(A) == 3
         A.add(x4)
         assert len(A) == 3
-        assert set(A._Unique_Heap__heap) == {x2, x3, x4}
+        assert set(A._Unique_Heapq__heap) == {x2, x3, x4}
 
     def test_pop(self, heap_item):
         x3 = Recursive_Heap_Item(
@@ -162,7 +244,7 @@ class Test_Unique_Heap:
             priority=3, last_element="h1", length=10, constraint_data=(1, 2)
         )
 
-        A = Unique_Heap()
+        A = Unique_Heapq()
         A.add(heap_item)
         A.add(x4)
         A.add(x3)
@@ -181,3 +263,90 @@ class Test_Unique_Heap:
         x = A.pop()
         assert x == x4
         assert len(A) == 0
+
+class Test_Inference:
+    # These are copied a pasted from test_oracle_chmm.py, I should do some refactoring probably
+    
+    
+    def test_a_star(self, app):
+        observed = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        print(recursive_a_star(hmm_app=app, observed=observed))
+        assert recursive_a_star(hmm_app=app, observed=observed).solutions[0].hidden == [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        ]
+        
+    
+    """
+    def test_a_star_2(self, chmm):
+        inference = Inference(statistical_model=chmm)
+        observed = ["o1", "o1", "o1", "o1", "o1", "o1", "o1", "o1", "o1", "o1"]
+        assert inference(observed).solutions[0].hidden == [
+            "h0",
+            "h0",
+            "h0",
+            "h0",
+            "h0",
+            "h0",
+            "h0",
+            "h0",
+            "h0",
+            "h0",
+        ]
+
+    def test_a_star_mult(self, chmm):
+        inference = Inference(statistical_model=chmm, num_solutions=2)
+        observed = ["o1", "o0", "o0", "o0", "o0", "o0", "o0", "o0", "o0", "o0", "o0"]
+        results = inference(observed)
+        assert results.termination_condition == "ok"
+        assert [sol.hidden for sol in results.solutions] == [
+            ["h0", "h0", "h0", "h0", "h0", "h0", "h0", "h0", "h0", "h0", "h0"],
+            ["h1", "h0", "h0", "h0", "h0", "h0", "h0", "h0", "h0", "h0", "h0"],
+        ]
+
+    def test_a_star_no_solution(self, chmm):
+        inference = Inference(statistical_model=chmm)
+        observed = ["o0"]
+        results = inference(observed)
+        assert results.termination_condition == "error: no feasible solutions"
+
+    def test_a_star_not_enough_solutions(self, chmm):
+        inference = Inference(statistical_model=chmm, num_solutions=2)
+        observed = ["o1", "o1", "o1", "o1", "o1", "o1", "o1", "o1", "o1", "o1"]
+        results = inference(observed)
+        assert results.termination_condition == "ok"
+
+    def test_a_star_deterministic_chmm(self):
+        start_probs = {"h0": 1, "h1": 0}
+        transition_probs = {
+            ("h0", "h0"): 0,
+            ("h0", "h1"): 1,
+            ("h1", "h0"): 0,
+            ("h1", "h1"): 1,
+        }
+        emission_probs = {
+            ("h0", "o0"): 1,
+            ("h0", "o1"): 0,
+            ("h1", "o0"): 0,
+            ("h1", "o1"): 1,
+        }
+        hmm = HMM()
+        hmm.load_model(
+            start_probs=start_probs,
+            transition_probs=transition_probs,
+            emission_probs=emission_probs,
+        )
+        hmm.set_seed(0)
+
+        observed = ["o0", "o1", "o1", "o1"]
+        inference = Inference(statistical_model=hmm)
+        assert inference(observed).solutions[0].hidden == ["h0", "h1", "h1", "h1"]
+    """
