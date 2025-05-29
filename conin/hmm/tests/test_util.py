d8e4f0762811309185f50c267f5dcac9e54f9216
clmatte <clmatte@sandia.gov>
2025-05-29 08:36:05 -0700

    Added util tests

--- conin/hmm/tests/test_util.py
+++ conin/hmm/tests/test_util.py
@@ -17,3 +17,21 @@ class Test_Util:
         assert Util.normalize_2d_dictionary(
             {("0", "0"): 1, ("0", "1"): 3, ("1", "0"): 1, ("1", "1"): -1}
         ) == {("0", "0"): 0.25, ("0", "1"): 0.75, ("1", "0"): 0.5, ("1", "1"): 0.5}
+
+    def test_sample_from_vec(self):
+        vec = [0.1, 0.3, 0.6]
+        for i in range(10):
+            assert Util.sample_from_vec(vec) in {0, 1, 2}
+
+    def test_normalize_vec(self):
+        vec = [1, 2, 3, 4, 6]
+        assert Util.normalize_vector(vec) == [1 / 16, 2 / 16, 3 / 16, 4 / 16, 6 / 16]
+        assert Util.normalize_vector([-1, 2, 0, -1]) == [1 / 4, 1 / 4, 1 / 4, 1 / 4]
+
+    def test_normalize_matrix(self):
+        mat = [[1, 2, 3, 4, 6], [1, -1, 1], [-1, 1]]
+        assert Util.normalize_matrix(mat) == [
+            [1 / 16, 2 / 16, 3 / 16, 4 / 16, 6 / 16],
+            [1, -1, 1],
+            [1 / 2, 1 / 2],
+        ]
