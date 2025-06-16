fcb172ac71de241d3842cf785679d66bf8671b72
whart222 <whart222@gmail.com>
2025-06-16 17:24:45 -0400

    Adding tests for DBNs

--- conin/inference/tests/test_OptimizationInference.py
+++ conin/inference/tests/test_OptimizationInference.py
@@ -76,6 +76,9 @@ def test_OptimizationInference_cancer2_ALL():
         "Xray": 0,
     }
 
+#
+# ConstrainedBayesianNetwork tests
+#
 
 def test_OptimizationInference_cancer2_constrained():
     pgm = conin.bayesian_network.tests.test_cases.cancer2_BN_constrained()
@@ -105,3 +108,99 @@ def test_OptimizationInference_cancer2_constrained():
         "Smoker": 0,
         "Xray": 0,
     }
+
+#
+# DynamicBayesianNetwork tests
+#
+
+def test_OptimizationInference_weather():
+    pgm = conin.dynamic_bayesian_network.tests.test_cases.pgmpy_weather2()
+    inf = OptimizationInference(pgm)
+
+    results = inf.map_query(solver='glpk')
+    assert results.solution.variable_value == {
+        ("H", 0): "Low",
+        ("H", 1): "Low",
+        ("H", 2): "Low",
+        ("H", 3): "Low",
+        ("H", 4): "Low",
+        ("O", 0): "Dry",
+        ("O", 1): "Dry",
+        ("O", 2): "Dry",
+        ("O", 3): "Dry",
+        ("O", 4): "Dry",
+        ("T", 0): "Hot",
+        ("T", 1): "Hot",
+        ("T", 2): "Hot",
+        ("T", 3): "Hot",
+        ("T", 4): "Hot",
+        ("W", 0): "Sunny",
+        ("W", 1): "Sunny",
+        ("W", 2): "Sunny",
+        ("W", 3): "Sunny",
+        ("W", 4): "Sunny",
+    }
+
+    evidence = {
+        ("O", 0): "Wet",
+        ("O", 1): "Wet",
+        ("O", 2): "Dry",
+        ("O", 3): "Dry",
+        ("O", 4): "Dry",
+        ("H", 0): "Medium",
+        ("H", 1): "Medium",
+        ("H", 2): "Medium",
+        ("H", 3): "Medium",
+        ("H", 4): "Medium",
+    }
+    results = inf.map_query(evidence=evidence, solver='glpk')
+    assert results.solution.variable_value == {
+        ("T", 0): "Hot",
+        ("T", 1): "Hot",
+        ("T", 2): "Mild",
+        ("T", 3): "Hot",
+        ("T", 4): "Hot",
+        ("W", 0): "Cloudy",
+        ("W", 1): "Rainy",
+        ("W", 2): "Sunny",
+        ("W", 3): "Sunny",
+        ("W", 4): "Sunny",
+    }
+
+#
+# ConstrainedDynamicBayesianNetwork tests
+#
+def test_OptimizationInference_weather_constrained():
+    pgm = conin.dynamic_bayesian_network.tests.test_cases.pgmpy_weather_constrained1()
+    inf = OptimizationInference(pgm)
+
+    results = inf.map_query(solver='glpk')
+    assert results.solution.variable_value == {
+    }
+
+    evidence = {
+        ("O", 0): "Wet",
+        ("O", 1): "Wet",
+        ("O", 2): "Dry",
+        ("O", 3): "Dry",
+        ("O", 4): "Dry",
+        ("H", 0): "Medium",
+        ("H", 1): "Medium",
+        ("H", 2): "Medium",
+        ("H", 3): "Medium",
+        ("H", 4): "Medium",
+    }
+    results = inf.map_query(evidence=evidence, solver='glpk')
+    assert results.solution.variable_value == {
+        ("T", 0): "Hot",
+        ("T", 1): "Mild",
+        ("T", 2): "Cold",
+        ("T", 3): "Hot",
+        ("T", 4): "Hot",
+        ("W", 0): "Rainy",
+        ("W", 1): "Rainy",
+        ("W", 2): "Sunny",
+        ("W", 3): "Sunny",
+        ("W", 4): "Sunny",
+    }
+
