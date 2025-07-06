a6cde79b44fd3ae7781cd6fe7151f29dbfa7986f
whart222 <whart222@gmail.com>
2025-07-06 14:35:25 -0600

    Delaying import of conin.inference

--- conin/__init__.py
+++ conin/__init__.py
@@ -1,11 +1,11 @@
 # conin.__init__.py
 
-from .inference import *
 from .common_constraints import *
 from . import markov_network
 from . import dynamic_bayesian_network
 from . import bayesian_network
 from . import hmm
+from .inference import *
 from .constraint import Constraint
 from .exceptions import InvalidInputError, InsufficientSolutionsError
 from . import __about__
