bf66bb3e107748a0942ea3f65b88b660801630ee
whart222 <whart222@gmail.com>
2025-07-06 14:40:30 -0600

    Several changes
    1. Using opaque option arguments, to allow arbitrary options to
    be passed-in to the model inference logic.
    
    2. More careful selection of when a Markov network is generated

--- conin/bayesian_network/inference.py
+++ conin/bayesian_network/inference.py
@@ -1,15 +1,34 @@
 try:
     import pgmpy.models
+    import pgmpy.inference
 except:
     pass
 from conin.markov_network import create_MN_map_query_model
 
 
 def create_BN_map_query_model(
-    *, pgm, variables=None, evidence=None, var_index_map=None, create_MN=False
+    *,
+    pgm,
+    variables=None,
+    evidence=None,
+    var_index_map=None,
+    **options,
 ):
+    prune_network = options.pop("prune_network", True)
+    create_MN = options.pop("create_MN", False)
 
-    if create_MN:
+    if prune_network:
+        inf = pgmpy.inference.Inference(pgm)
+        pgm, evidence = inf._prune_bayesian_model(
+            [] if variables is None else variables, evidence
+        )
+
+    if variables or evidence or create_MN:
+        #
+        # We need to create a Markov network if (1) the user asks us to, or (2) we have 'variables' or
+        # 'evidence' specified.  In (2), we need to reduce the factors and apply variable elimination to
+        # eliminate unspecified variables.
+        #
         MN = pgm.to_markov_model()
     else:
         #
