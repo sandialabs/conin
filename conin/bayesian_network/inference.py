ed3bc98858cb991589c5b041344bfb5d9db039a9
whart222 <whart222@gmail.com>
2025-07-07 06:53:20 -0600

    Adding citation for pruning methodology

--- conin/bayesian_network/inference.py
+++ conin/bayesian_network/inference.py
@@ -18,6 +18,14 @@ def create_BN_map_query_model(
     create_MN = options.pop("create_MN", False)
 
     if prune_network:
+        """
+        Apply the pruning methods described here:
+
+        M. Baker, T. E. Boult. 1990. Pruning bayesian networks for efficient computation. 
+        In Proc. of the Sixth Annual Conf. on Uncertainty in Artificial Intelligence (UAI '90). 
+        Elsevier Science Inc., USA, 225–232.
+        https://arxiv.org/abs/1304.1112
+        """
         inf = pgmpy.inference.Inference(pgm)
         pgm, evidence = inf._prune_bayesian_model(
             [] if variables is None else variables, evidence
