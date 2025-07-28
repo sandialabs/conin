7dde3376b9b17d21d3fd474579cacbcf89f48e1b
whart222 <whart222@gmail.com>
2025-07-28 08:44:45 -0600

    Adding solver_options argument

--- conin/markov_network/inference.py
+++ conin/markov_network/inference.py
@@ -204,8 +204,9 @@ def create_MN_map_query_model_from_factorial_repn(
     return model
 
 
-def optimize_map_query_model(model, *, solver="gurobi", tee=False, with_fixed=False):
+def optimize_map_query_model(model, *, solver="gurobi", tee=False, with_fixed=False, solver_options=None):
     opt = pe.SolverFactory(solver)
+    opt.options = solver_options
     res = opt.solve(model, tee=tee)
     pe.assert_optimal_termination(res)
 
