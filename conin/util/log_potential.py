ec4930a700e066875146073e0c4d18af9a291af9
whart222 <whart222@gmail.com>
2025-07-29 03:25:30 -0600

    Renaming a file

--- conin/util/log_potential.py
+++ conin/util/log_potential.py
@@ -7,6 +7,10 @@ except:
 
 
 def log_potential(pgm, query_variables, evidence=None, options**):
+    """
+    A wrapper function that calls log_potential functions for specific libraries that
+    conin interfaces with.
+    """
     if pgmpy_available:
         if isinstance(pgm, pgmpy.models.DiscreteBayesianNetwork) or isinstance(pgm, pgmpy.models.MarkovNetwork):
             return conin.util.pgmpy.log_potential(pgm, query_variables, evidence=evidence, **options)
