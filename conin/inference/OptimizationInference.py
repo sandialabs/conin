c3c14a89fd7b48b80a25e6d4f7b948cf96880687
whart222 <whart222@gmail.com>
2025-08-29 08:27:16 -0600

    Simplifying logic with pgmpy models

--- conin/inference/OptimizationInference.py
+++ conin/inference/OptimizationInference.py
@@ -68,33 +68,20 @@ class IntegerProgrammingInference:
         >>> inference = OptimizationInference(model)
         >>> phi_query = inference.map_query(variables=['A', 'B'])
         """
-        if (
-            isinstance(self.pgm, DiscreteMarkovNetwork)
-            or isinstance(self.pgm, ConstrainedDiscreteMarkovNetwork)
-            or isinstance(self.pgm, DiscreteBayesianNetwork)
-            or isinstance(self.pgm, ConstrainedDiscreteBayesianNetwork)
+        if isinstance(self.pgm, pgmpy.models.MarkovNetwork) or isinstance(
+            self.pgm, pgmpy.models.DiscreteBayesianNetwork
         ):
-            model = self.pgm.create_map_query_model(
-                variables=variables,
-                evidence=evidence,
-                timing=timing,
-                **options,
-            )
-            return optimize_map_query_model(model, timing=timing, **options)
+            pgm = convert_pgmpy_to_conin(self.pgm)
+        else:
+            pgm = self.pgm
 
-        elif isinstance(self.pgm, pgmpy.models.MarkovNetwork):
-            model = create_MN_map_query_model(
-                pgm=convert_pgmpy_to_conin(self.pgm),
-                variables=variables,
-                evidence=evidence,
-                timing=timing,
-                **options,
-            )
-            return optimize_map_query_model(model, timing=timing, **options)
-
-        elif isinstance(self.pgm, pgmpy.models.DiscreteBayesianNetwork):
-            model = create_BN_map_query_model(
-                pgm=convert_pgmpy_to_conin(self.pgm),
+        if (
+            isinstance(pgm, DiscreteMarkovNetwork)
+            or isinstance(pgm, ConstrainedDiscreteMarkovNetwork)
+            or isinstance(pgm, DiscreteBayesianNetwork)
+            or isinstance(pgm, ConstrainedDiscreteBayesianNetwork)
+        ):
+            model = pgm.create_map_query_model(
                 variables=variables,
                 evidence=evidence,
                 timing=timing,
@@ -149,20 +136,15 @@ class DDBN_IntegerProgrammingInference:
         >>> phi_query = inference.map_query(variables=['A', 'B'])
         """
 
-        if isinstance(self.pgm, DynamicDiscreteBayesianNetwork) or isinstance(
-            self.pgm, ConstrainedDynamicDiscreteBayesianNetwork
-        ):
-            model = self.pgm.create_map_query_model(
-                start=start, stop=stop, variables=variables, evidence=evidence
-            )
-            return optimize_map_query_model(model, **options)
+        if isinstance(self.pgm, pgmpy.models.DynamicBayesianNetwork):
+            pgm = convert_pgmpy_to_conin(self.pgm)
+        else:
+            pgm = self.pgm
 
-        elif isinstance(self.pgm, pgmpy.models.DynamicBayesianNetwork):
-            model = create_DDBN_map_query_model(
-                pgm=convert_pgmpy_to_conin(self.pgm),
-                start=start,
-                stop=stop,
-                variables=variables,
-                evidence=evidence,
+        if isinstance(pgm, DynamicDiscreteBayesianNetwork) or isinstance(
+            pgm, ConstrainedDynamicDiscreteBayesianNetwork
+        ):
+            model = pgm.create_map_query_model(
+                start=start, stop=stop, variables=variables, evidence=evidence
             )
             return optimize_map_query_model(model, **options)
