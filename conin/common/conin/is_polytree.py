e602086917c1e6b6e047ad47d55abfa592c70dfd
whart222 <whart222@gmail.com>
2025-12-19 14:56:59 -0700

    Several changes
    1. Changing semantics of conin.common
    
       Imports now pull-in functions for conin models, rather than
       the unified functions in conin.common.unified.  These can be
       pulled-in directly as needed.
    
    2. Adding a is_polytree() function for conin BNs.
    
    3. Adding is_polytree tests.

---
+++ conin/common/conin/is_polytree.py
@@ -0,0 +1,51 @@
+from collections import deque
+from conin.bayesian_network import DiscreteBayesianNetwork
+
+
+def is_polytree(pgm):
+    """
+    Takes in a conin BN model and returns whether it is a polytree.
+    If a BN is a polytree, then belief propogation is an exact algorithm.
+
+    Does a BFS.
+
+    Inputs:
+        - pgm: conin discrete BN model
+    Output:
+        - bool
+    """
+    if not(type(pgm) is DiscreteBayesianNetwork):
+        raise TypeError(f"is_polytree() expects an argument of type DiscreteBayesianNetwork ({type(pgm)=})")
+
+    nodes = set(pgm.nodes)
+    nbrs = {node: set() for node in nodes}
+
+    for cpd in pgm.cpds:
+        if cpd.parents:
+            for p in cpd.parents:
+                nbrs[p].add(cpd.node)
+                nbrs[cpd.node].add(p)
+
+    visited = set()
+    queue = deque()
+
+    while True:
+        if len(queue) == 0:
+            if len(visited) == len(nodes):
+                return True
+            else:
+                # WEH - Do we just need to pick any member of the set: set(nodes) - visited?
+                #queue.append(list(nodes - visited)[0])
+                queue.append( next(iter(nodes - visited)) )
+
+        else:
+            u = queue.popleft()
+
+            if nbrs[u] & set(queue):
+                return False
+
+            visited.add(u)
+
+            for v in nbrs[u]:
+                if v not in visited:
+                    queue.append(v)
--- conin/common/unified/__init__.py
+++ conin/common/unified/__init__.py
@@ -1,4 +1,4 @@
 from .load_model import load_model
 from .save_model import save_model
 from .log_potential import log_potential
-from .is_polytree import is_polytree
+#from .is_polytree import is_polytree
