7b944e1e207fc27e758f95d7e1b647dec3d7f013
whart222 <whart222@gmail.com>
2025-12-22 07:45:28 -0700

    Updating reference link

--- conin/bayesian_network/tests/examples.py
+++ conin/bayesian_network/tests/examples.py
@@ -512,8 +512,8 @@ def holmes_pgmpy(debug=False):
     E - Does an earthquake happen?
     R - Does he hear about an earthquake on the radio?
 
-    A. Gao. "Lecture 13: Variable Elimination Algorithm", 2021.
-    https://cs.uwaterloo.ca/~a23gao/cs486686_f18/schedule.shtml
+    A. Gao. "Lecture 12: Variable Elimination Algorithm", 2021.
+    https://cs.uwaterloo.ca/~a23gao/cs486686_f18/slides/lec12_inferences_bayes_nets_nosol.pdf
     """
     G = pgmpy_DiscreteBayesianNetwork()
     G.add_nodes_from(["E", "B", "R", "A", "W", "G"])
