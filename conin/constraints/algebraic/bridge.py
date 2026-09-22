c832ea8750bd9184d38ebc64374925ab093cd0e2
whart222 <whart222@gmail.com>
2026-09-22 05:34:40 -0600

    Bug fix

--- conin/constraints/algebraic/bridge.py
+++ conin/constraints/algebraic/bridge.py
@@ -11,7 +11,7 @@ from conin.util import try_import
 with try_import() as smoek_available:
     from smoek.core.expr.nodes import ExprLeaf
 if not smoek_available:
-    class ExprLeaf(objecti): pass
+    class ExprLeaf(object): pass
 
 class ConinVarNode(ExprLeaf):
     """
